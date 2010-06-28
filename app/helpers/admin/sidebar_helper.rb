da666b73ac8e7827474ab1ff9d63e1602a9359a2
Ken Miller <ken@corp.mamasource.com>
2010-06-28 11:06:59 -0700

    fixing bug causing wrong filter item to show as selected

--- app/helpers/admin/sidebar_helper.rb
+++ app/helpers/admin/sidebar_helper.rb
@@ -125,7 +125,7 @@ module Admin::SidebarHelper
       related_items = model.find(:all, :order => model.typus_order_by)
       if related_items.size > model.typus_options_for(:sidebar_selector)
         related_items.each do |item|
-          switch = 'selected' if request.include?("#{related_fk}=#{item.id}")
+          switch = 'selected' if request =~ /#{related_fk}=#{item.id}\b/i
           items << <<-HTML
 <option #{switch} value="#{url_for params.merge(related_fk => item.id, :page => nil)}">#{truncate(item.to_label, :length => 25)}</option>
           HTML
